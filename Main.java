21aa0dc44c2221f23472048dfab1a21b38d7938d
MiroEn <klosse@abv.bg>
2024-09-19 18:15:41 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -25,6 +25,8 @@ public class Main {
                 case "GetInterest":
                     getInterest(Integer.parseInt(line[1]), Integer.parseInt(line[2]), bankAccounts);
                     break;
+                default:
+                    System.out.println("Just testing")
             }
         
         }
